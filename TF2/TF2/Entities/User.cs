fc07590f45fa9e55d31d48caabe232f773c2e482
Elina <elina815@gmail.com>
2019-11-21 20:01:05 +0200

    db

--- TF2/TF2/Entities/User.cs
+++ TF2/TF2/Entities/User.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace TF2.Entities
 {
+    [Table ("Users")]
     public class User
     {
+        [PrimaryKey, AutoIncrement, Column("Id")]
         public int Id { get; set; }
+        [Column("Username")]
         public string Username { get; set; }
+        [Column("Password")]
         public string Password { get; set; }
+        [Column("Email")]
         public string Email { get; set; }
+        [Column("Role")]
         public string Role { get; set; }
     }
 }
