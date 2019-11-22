337b0bffa53f0886fa755f470e3d3b57f89f1773
Elina <elina815@gmail.com>
2019-11-22 22:16:22 +0200

    db connection

--- TF2/TF2/Entities/EntityLoader.cs
+++ TF2/TF2/Entities/EntityLoader.cs
@@ -2,12 +2,43 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Collections.Generic;
-
+using System.IO;
+using Android.App;
+using SQLite;
 
 namespace TF2.Entities
 {
+    //copies the DB from Assets to the local device and loads the entity lists from it
     public class EntityLoader
     {
+        public static List<Lecturer> lecturers = new List<Lecturer>();
+        public static List<Subject> subjects = new List<Subject>();
+        public static List<Review> reviews = new List<Review>();
+
+        public static void ConnectToDatabase(string dbName)
+        {
+            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), dbName);
+
+            //copy DB to device
+            if (!File.Exists(dbPath))
+            {
+                using (var br = new BinaryReader(Application.Context.Assets.Open(dbName)))
+                {
+                    using (var bw = new BinaryWriter(new FileStream(dbPath, FileMode.Create)))
+                    {
+                        byte[] buffer = new byte[2048];
+                        int length = 0;
+                        while ((length = br.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            bw.Write(buffer, 0, length);
+                        }
+                    }
+                }
+            }
+
+            var db = new SQLiteConnection(dbPath);
+        }
+
 
     }
 }
